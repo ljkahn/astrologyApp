0b7ced5d3f2c9a0f8f5a0beb58c67e90556f4d4f
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-23 13:45:50 -0600

    added basic ajax api call

---
+++ index.js
@@ -0,0 +1,47 @@
+
+$(function () {
+
+
+  const settings = {
+    'url': 'https://zylalabs.com/api/1508/astrological+calculation+api/1243/birthdata',
+    'method': 'POST',
+    'timeout': 0,
+    'headers': {
+      'Authorization': '2229|TaIc6CtmSFuhyqTAqSwbW6fOFtWDlmVPTKbRo8ZJ'
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
