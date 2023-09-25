933ea0643a3874d2b182c6a66fd2dd65e64fe0f1
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-25 09:24:21 -0600

    merge backup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 $(function () {
 
 
+  const btnEl = $("#submit");
+
   const settings = {
     'url': 'https://zylalabs.com/api/1508/astrological+calculation+api/1243/birthdata',
     'method': 'POST',
@@ -9,7 +11,16 @@ $(function () {
     'headers': {
       'Authorization': '2229|TaIc6CtmSFuhyqTAqSwbW6fOFtWDlmVPTKbRo8ZJ'
     },
+    "data": "{\r\n    \"name\": \"Test\",\r\n    \"year\": 2000,\r\n    \"month\": 10,\r\n    \"day\": 10,\r\n    \"hour\": 10,\r\n    \"minute\": 10,\r\n    \"longitude\": 45,\r\n    \"latitude\": 45,\r\n    \"city\": \"Roma\",\r\n    \"timezone\": \"Europe\/Rome\",\r\n    \"language\": \"IT\"\r\n}",
+
+
   };
+
+
+
+
+
+
 
   $.ajax(settings).done(function (response) {
     console.log(response);
@@ -17,12 +28,22 @@ $(function () {
 
 
 
+  function data() {
+    const requestLink = settings.url;
+    fetch(requestLink)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  };
 
 
 
+  //event listeners
 
-
-
+  btnEl.on("click", data());
 
 
 
